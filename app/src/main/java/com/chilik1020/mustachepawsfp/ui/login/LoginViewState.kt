f7a4c5e6fbdeea3f94b69636e6e4aa0cfc1c638e
Andrey Demchuk <avdemchuk@gmail.com>
2020-12-04 14:48:53 +0300

    Added LoginViewModel, AuthInteractor, LoginInteractor

--- app/src/main/java/com/chilik1020/mustachepawsfp/ui/login/LoginViewState.kt
+++ app/src/main/java/com/chilik1020/mustachepawsfp/ui/login/LoginViewState.kt
@@ -1,10 +1,9 @@
 package com.chilik1020.mustachepawsfp.ui.login
 
 sealed class LoginViewState {
-
     object LoginLoadingState : LoginViewState()
-    object LoggedState: LoginViewState()
-    class LoginErrorState(val message : String) : LoginViewState()
-    class UsernameErrorState(val message : String) : LoginViewState()
-    class PasswordErrorState(val message : String) : LoginViewState()
+    object LoggedState : LoginViewState()
+    class LoginErrorState(val message: String) : LoginViewState()
+    class UsernameErrorState(val message: String) : LoginViewState()
+    class PasswordErrorState(val message: String) : LoginViewState()
 }
