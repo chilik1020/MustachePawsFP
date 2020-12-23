4af931468e40807d9fe885bf5b0f6173a850749f
Andrey Demchuk <avdemchuk@gmail.com>
2020-12-23 22:50:42 +0300

    Changed PostListFragment: added Adpater, recyclerView init. render fun

--- presentation/src/main/java/com/chilik1020/mustachepawsfp/ui/postlist/PostListFragment.kt
+++ presentation/src/main/java/com/chilik1020/mustachepawsfp/ui/postlist/PostListFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.chilik1020.mustachepawsfp.databinding.FragmentPostListBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
@@ -16,6 +18,7 @@ class PostListFragment : DaggerFragment() {
 
     lateinit var binding: FragmentPostListBinding
     lateinit var viewModel: PostListViewModel
+    private val postListAdapter = PostListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,14 +35,31 @@ class PostListFragment : DaggerFragment() {
     }
 
     private fun initViews() {
+        binding.rvPostList.apply {
+            adapter = postListAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
         viewModel.viewState.observe(viewLifecycleOwner) { render(it) }
     }
 
     private fun render(state: PostListViewState) {
         when (state) {
-            PostListViewState.Loading -> TODO()
-            is PostListViewState.Success -> TODO()
-            is PostListViewState.Error -> TODO()
+            PostListViewState.Loading -> {
+                binding.pbFetchPostsLoading.visibility = View.VISIBLE
+            }
+            is PostListViewState.Success -> {
+                binding.pbFetchPostsLoading.visibility = View.GONE
+                postListAdapter.setData(state.data)
+            }
+            is PostListViewState.Error -> {
+                binding.pbFetchPostsLoading.visibility = View.GONE
+                showSnackBarMessage(state.msg)
+            }
         }
+    }
+
+    private fun showSnackBarMessage(msg: String) {
+        Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
     }
 }
